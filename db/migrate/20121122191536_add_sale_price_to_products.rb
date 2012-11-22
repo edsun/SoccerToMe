c14afc46238f1761f8e91c911d917c90fdd43372
edsun <edwardclorina@gmail.com>
2012-11-22 13:16:18 -0600

    Added sale_price column to products

---
+++ db/migrate/20121122191536_add_sale_price_to_products.rb
@@ -0,0 +1,6 @@
+class AddSalePriceToProducts < ActiveRecord::Migration
+  def change
+    add_column :products, :sale_price, :decimal
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121122191320) do
+ActiveRecord::Schema.define(:version => 20121122191536) do
 
   create_table "addresses", :force => true do |t|
     t.boolean  "main_address"
@@ -86,6 +86,7 @@ ActiveRecord::Schema.define(:version => 20121122191320) do
     t.datetime "updated_at",      :null => false
     t.integer  "category_id"
     t.integer  "manufacturer_id"
+    t.decimal  "sale_price"
   end
 
   create_table "provinces", :force => true do |t|
